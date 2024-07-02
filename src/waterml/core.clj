1e5780116d94cddf9ea70887b391e6dc7fe8dca8
gwrxuk <gwrx2005@gmail.com>
2024-07-02 10:09:15 +0800

    Read CSV

--- src/waterml/core.clj
+++ src/waterml/core.clj
@@ -1,6 +1,16 @@
-(ns waterml.core
+(ns waterml.core 
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io])
   (:gen-class))
+
+(defn read-csv [file-path]
+  (with-open [reader (io/reader file-path)]
+    (doall
+      (csv/read-csv reader))))
 
 (defn -main
   [& args]
+    (let [file-path "data/waterlevel.csv"
+        data (read-csv file-path)]
+    (println data))
   (println "Predict Water Level"))
